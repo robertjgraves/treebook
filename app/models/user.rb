f24fae6d708a24fd82e0a62472af7cff68bec5cc
Robert Graves <robertjgraves@gmail.com>
2013-09-04 13:50:43 -0700

    Styled the flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ActiveRecord::Base
   validates :profile_name, presence: true,
                            uniqueness: true,
                            format: {
-                            with: /a-zA-Z0-9_-/,
-                            message: 'must be formatted correctly. No spaces!'
+                            with: /\A[a-zA-Z\-\_]+\Z/,
+                            message: "must be formatted correctly. No spaces!"
                            }
   has_many :statuses
   
