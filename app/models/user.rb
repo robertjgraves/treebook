8506410c95176d2e8962f2213b648e45ea59e858
Robert Graves <robertjgraves@gmail.com>
2013-10-10 10:03:44 -0700

    Added user friendships and associated tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
+
   attr_accessible :email, :password, :password_confirmation, :remember_me,
   				  :first_name, :last_name, :profile_name
-  # attr_accessible :title, :body
+
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -19,7 +16,9 @@ class User < ActiveRecord::Base
                             message: "must be formatted correctly. No spaces!"
                            }
   has_many :statuses
-  
+  has_many :user_friendships
+  has_many :friends, through: :user_friendships
+
   def full_name
   	first_name + " " + last_name
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships) 
+  should have_many(:friends)
+
   test "a user should enter a first name" do 
   	user = User.new
   	assert !user.save
@@ -47,5 +50,17 @@ class UserTest < ActiveSupport::TestCase
     assert user.valid?
   end
 
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:robert).friends
+    end
+  end
 
+  test "that creating friendships on a user works" do
+    users(:robert).friends << users(:william)
+    users(:robert).friends.reload
+    assert users(:robert).friends.include?(users(:william))
+  end
+
+  
 end
