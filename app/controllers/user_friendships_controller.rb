020c8d20ceac3ed6fe61a851c579f7d521ebf993
Robert Graves <robertjgraves@gmail.com>
2013-10-26 17:57:01 -0700

    Added some ajax sprinkles.

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -1,8 +1,10 @@
 class UserFriendshipsController < ApplicationController
 	before_filter :authenticate_user!
+	respond_to :html, :json
 	
 	def index
 		@user_friendships = current_user.user_friendships.all
+		respond_with @user_friendships
 	end
 
 	def accept
@@ -34,12 +36,23 @@ class UserFriendshipsController < ApplicationController
 		if params[:user_friendship] && params[:user_friendship].has_key?(:friend_id)
 			@friend = User.where(profile_name: params[:user_friendship][:friend_id]).first
 			@user_friendship = UserFriendship.request(current_user, @friend)
-			if @user_friendship.new_record?
-				flash[:error] = "There was a problem creating that friend request."
-			else
-				flash[:success] = "Friend request sent."
+			
+			respond_to do |format|
+				if @user_friendship.new_record?
+					format.html do 
+						flash[:error] = "There was a problem creating that friend request."
+						redirect_to profile_path(@friend)
+					end
+					format.json { render json: @user_friendship.to_json, status: :precondition_failed }
+				else
+					format.html do
+						flash[:success] = "Friend request sent."
+						redirect_to profile_path(@friend)
+					end
+					format.json { render json: @user_friendship.to_json }
+				end
+				
 			end
-			redirect_to profile_path(@friend)
 		else
 			flash[:error] = "Friend required"
 			redirect_to root_path
@@ -48,16 +61,14 @@ class UserFriendshipsController < ApplicationController
 
 	def edit
 		@friend = User.where(profile_name: params[:id]).first
-    	@user_friendship = current_user.user_friendships.where(friend_id: @friend.id).first
+   		@user_friendship = current_user.user_friendships.where(friend_id: @friend.id).first.decorate
 	end
 
 	def destroy
 		@user_friendship = current_user.user_friendships.find(params[:id])
-    	
     	if @user_friendship.destroy
-     		 flash[:success] = "Friendship destroyed"
-   		end
+      		flash[:success] = "Friendship destroyed"
+    	end
     	redirect_to user_friendships_path
-
 	end
 end
